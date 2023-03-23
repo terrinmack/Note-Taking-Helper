e47c9b2120d1993989019de7dc9dd5c7dbc22764
Your Name <you@example.com>
2023-03-22 20:10:22 -0700

    edited file paths and edited code in server.js file

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
