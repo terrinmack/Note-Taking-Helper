2148042f65e2a4ae89d4ce37f7fbef22d925838f
Your Name <terrimack15@gmail.com>
2023-05-22 16:00:08 -0700

    working on server.js and index.js within routes folder

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const notesRoute = require('./notes');
+
+const app = express();
+
+app.use('/notes', notesRoute);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -3,14 +3,23 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
-
 const PORT = process.env.port || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+
+app.use('/api', api);
+
+// GET for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// GET for /notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Now listening on PORT: ${PORT}`);
