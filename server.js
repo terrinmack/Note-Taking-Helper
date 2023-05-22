efdefb5d04f5187e3f573a19ba645bd15bae09c2
Your Name <terrimack15@gmail.com>
2023-05-22 15:55:43 -0700

    going to simplify folders. Removing routes to have everything be in server.js instead

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 
+const PORT = process.env.port || 3001;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
